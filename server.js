a5694b9fe1d08b9bb442ddbf80a9bbb605eb76ea
Kevin <kevin.lin@leinotek.com>
2024-10-21 17:55:36 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const routes = require("./routes");
+const cors = require("cors");
+const port = process.env.PORT || 8080;
+
+//連結 mongoDB
+mongoose
+  .connect(process.env.MONGODB_CONNECTION)
+  .then(() => {
+    console.log("連結到 mongoDB");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//Handle Router
+//app.use("/api/lotto", routes.lotto);
+
+//監聽 http request
+app.listen(port, () => {
+  console.log("後端伺服器聆聽中....");
+});
