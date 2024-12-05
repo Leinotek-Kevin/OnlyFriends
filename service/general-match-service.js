1b184ca2db13ac855f7b3a71954f1cc164cca01c
Kevin <kevin.lin@leinotek.com>
2024-12-05 14:57:23 +0800

    添加配對的排程服務

---
+++ service/general-match-service.js
@@ -0,0 +1,204 @@
+const User = require("../models").user;
+const UserRelation = require("../models").userRelation;
+const Config = require("../models").config;
+const MatchHistory = require("../models").matchHistory;
+const MatchNewest = require("../models").matchNewest;
+const dateUtil = require("../utils/date-util");
+
+//執行一般配對
+const runGeneralMatch = async () => {
+  const RE_MATCH_DELAY = 48 * 60 * 60 * 1000;
+  const time48HoursAgo = Date.now() - RE_MATCH_DELAY; // 計算48小時前的時間點
+  const lastNightTimeStamp = dateUtil.getYesterdayNight();
+
+  try {
+    console.log("開始執行配對");
+
+    let time = Date.now();
+
+    //標記正在配對的狀態
+    await Config.updateOne({ matchScheduleStatus: "1" });
+
+    //清空最近的配對列表
+    await MatchNewest.deleteMany({});
+
+    //刪除大於過去兩天的歷史配對紀錄
+    await MatchHistory.deleteMany({
+      matchDate: { $lt: time48HoursAgo },
+    });
+
+    //儲存本次配對次數已用完的用戶
+    let consumeUsers = new Set();
+
+    //只有存活的用戶可以配對:昨天有上線的用戶即可
+    const aliveUsers = await User.find({
+      lastLoginTime: { $gte: lastNightTimeStamp },
+      identity: 2,
+    });
+
+    //執行有存活的訂閱用戶(訂閱用戶可以配對三個存活用戶/非訂閱只能一個)
+    for (let i = 0; i < aliveUsers.length; i++) {
+      const currentUser = aliveUsers[i];
+
+      //如果這個用戶已經用完配對次數，就換下一個用戶執行
+      if (consumeUsers.has(currentUser.userID)) {
+        continue;
+      }
+
+      //查詢這個用戶最近48hr的配對紀錄
+      const lastMatches = await MatchHistory.find({
+        matchDate: { $gte: time48HoursAgo }, // 過去48小時到現在的配對
+        $or: [{ user1ID: currentUser.userID }, { user2ID: currentUser.userID }],
+      });
+
+      // 提取與 currentUser 最近48hr配對過的用戶ID
+      const lastMatchedUserIds = lastMatches.map((match) =>
+        match.user1ID === currentUser.userID ? match.user2ID : match.user1ID
+      );
+
+      //這個用戶封鎖的對象
+      let relation = await UserRelation.findOne({
+        userID: currentUser.userID,
+      });
+      const unlikeObjects = relation == null ? [] : relation.unlikeUsers;
+
+      //檢查目前這個用戶還剩下多少配對次數,沒訂閱不用檢查,因為只有一次
+      let targetUserCount = 0;
+
+      if (currentUser.isSubscription) {
+        //檢查訂閱用戶剩餘次數
+        const matchs = await MatchNewest.find({
+          $or: [
+            { user1ID: currentUser.userID },
+            { user2ID: currentUser.userID },
+          ],
+        });
+        //剩下配對的次數
+        targetUserCount = 3 - matchs.length;
+      } else {
+        targetUserCount = 1;
+      }
+
+      //合併排除的對象
+      const consumeSetArray = Array.from(consumeUsers);
+      const combinedArray = [
+        ...lastMatchedUserIds,
+        ...consumeSetArray,
+        ...unlikeObjects,
+      ];
+
+      //用戶篩選條件
+      let {
+        objectCondition: {
+          objectGender,
+          objectAge: { maxAge, minAge },
+          objectRegion,
+        },
+      } = currentUser;
+
+      maxAge = maxAge == 50 ? 200 : maxAge;
+
+      //聚合篩選條件
+      const targetUsers = await User.aggregate([
+        {
+          $match: {
+            userID: {
+              $ne: currentUser.userID,
+              $nin: Array.from(combinedArray), //排除曾經配對及已經用光配對次數的用戶
+            },
+            lastLoginTime: { $gte: lastNightTimeStamp },
+          },
+        },
+        {
+          $addFields: {
+            genderScore: {
+              $cond: [{ $eq: ["$userGender", objectGender] }, 1, 0],
+            },
+            ageScore: {
+              $cond: [
+                {
+                  $and: [
+                    { $gte: ["$userAge", minAge] },
+                    { $lte: ["$userAge", maxAge] },
+                  ],
+                },
+                1,
+                0,
+              ],
+            },
+            regionScore: {
+              $cond: [{ $eq: ["$userRegion", objectRegion] }, 1, 0],
+            },
+          },
+        },
+        {
+          $sort: {
+            identity: -1, //依照用戶識別->降序 2:真人 ,1：官方, 0：假人
+            genderScore: -1, //性別匹配的加權分數，降序
+            ageScore: -1, // 年齡匹配的加權分數，降序
+            regionScore: -1, // 地區匹配的加權分數，降序
+          },
+        },
+        { $limit: targetUserCount },
+      ]);
+
+      consumeUsers.add(currentUser.userID);
+
+      const matches = [];
+
+      //紀錄配對
+      for (let i = 0; i < targetUsers.length; i++) {
+        let targetUser = targetUsers[i];
+
+        let url =
+          Number(currentUser.userID) > Number(targetUser.userID)
+            ? `${targetUser.userID}_${currentUser.userID}`
+            : `${currentUser.userID}_${targetUser.userID}`;
+
+        let match = {
+          user1ID: currentUser.userID, // 確保存儲 user1ID
+          user2ID: targetUser.userID, // 確保存儲 user2ID
+          user1_ID: currentUser._id, // 確保存儲 user1_ID
+          user2_ID: targetUser._id, // 確保存儲 user2_ID
+          sendbirdUrl: url,
+          isChecked: false,
+          matchUIType: 1,
+        };
+
+        if (targetUser.isSubscription) {
+          //如果目標對象是訂閱用戶，就要額外檢查他的配對次數
+          const targetAlreadyMatches = await MatchNewest.find({
+            $or: [
+              { user1ID: targetUser.userID },
+              { user2ID: targetUser.userID },
+            ],
+          });
+
+          if (targetAlreadyMatches.length == 2) {
+            //如果這個用戶已經兩次了，那這個迴圈就是他的最後一次機會,就加入 consumeSet
+            consumeUsers.add(targetUser.userID);
+          }
+        } else {
+          //如果目標對象是非訂閱用戶，直接加入 consumeSet 即可,因為他只有一次機會
+          consumeUsers.add(targetUser.userID);
+        }
+
+        matches.push(match);
+      }
+
+      await MatchNewest.insertMany(matches);
+      await MatchHistory.insertMany(matches);
+    }
+
+    //標記正在配對的狀態
+    await Config.updateOne({ matchScheduleStatus: "2" });
+
+    let finishTime = (Date.now() - time) / 1000.0;
+
+    console.log("已完成配對！" + finishTime);
+  } catch (error) {
+    console.log("配對出現問題" + error);
+  }
+};
+
+runGeneralMatch();
---
+++ service/letter-match-service.js
@@ -0,0 +1,137 @@
+const MatchNewest = require("../models").matchNewest;
+const EmotionLetter = require("../models").letter;
+const dateUtil = require("../utils/date-util");
+
+const runLetterMatch = async () => {
+  try {
+    let time = Date.now();
+
+    const lastNightTimeStamp = dateUtil.getYesterdayNight();
+    const todayNightTimeStamp = dateUtil.getTodayNight();
+
+    //昨天午夜到現在午夜的信封
+    const allowLettersCount = await EmotionLetter.countDocuments({
+      createTime: { $gte: lastNightTimeStamp, $lt: todayNightTimeStamp },
+    });
+
+    // 根據總數量決定百分比
+    // 100人->20% ,1000人->15% , 5000人-> 10%
+    let percentage;
+    if (allowLettersCount <= 100) {
+      percentage = 0.2;
+    } else if (allowLettersCount <= 1000) {
+      percentage = 0.15;
+    } else if (allowLettersCount <= 5000) {
+      percentage = 0.1;
+    }
+
+    // 計算應該配對的數量
+    const sampleSize = Math.ceil((allowLettersCount * percentage) / 2);
+
+    // 使用 MongoDB 的聚合管道來隨機取樣
+    let randomLetters = await EmotionLetter.aggregate([
+      {
+        $match: {
+          createTime: {
+            $gte: lastNightTimeStamp, // 大於等於昨晚的時間
+            $lt: todayNightTimeStamp, // 小於今天晚上的時間
+          },
+        },
+      },
+      {
+        $sample: { size: sampleSize }, // 隨機取樣
+      },
+    ]);
+
+    let letterMatches = [];
+
+    //儲存本次配對次數已用完的信封
+    let consumeUsers = new Set();
+
+    for (let i = 0; i < randomLetters.length; i++) {
+      //當前候選信封
+      const currentLetter = randomLetters[i];
+
+      //如果這個信封已經用完配對次數，就換下一個信封執行
+      if (consumeUsers.has(currentLetter.letterUserID)) {
+        continue;
+      }
+
+      //查詢這個信封在今天午夜配對的紀錄
+      const lastMatches = await MatchNewest.find({
+        $or: [
+          { user1ID: currentLetter.letterUserID },
+          { user2ID: currentLetter.letterUserID },
+        ],
+      });
+
+      // 過濾 currentLetter 今天午夜配對的對象
+      const lastMatchedUserIds = lastMatches.map((match) =>
+        match.user1ID === currentLetter.letterUserID
+          ? match.user2ID
+          : match.user1ID
+      );
+
+      //合併不能被配對到的對象
+      const consumeSetArray = Array.from(consumeUsers);
+      const combinedArray = [...lastMatchedUserIds, ...consumeSetArray];
+
+      //找尋目標用戶
+      const targetLetters = await EmotionLetter.aggregate([
+        {
+          $match: {
+            letterUserID: {
+              $ne: currentLetter.letterUserID,
+              $nin: Array.from(combinedArray), //排除曾經配對及已經用光配對次數的用戶
+            },
+            createTime: {
+              $gte: lastNightTimeStamp, // 大於等於昨晚的時間
+              $lt: todayNightTimeStamp, // 小於今天晚上的時間
+            },
+          },
+        },
+        { $limit: 1 },
+      ]);
+
+      if (targetLetters != null && targetLetters.length > 0) {
+        consumeUsers.add(targetLetters[0].letterUserID);
+        consumeUsers.add(currentLetter.letterUserID);
+
+        //紀錄配對用戶
+        let url =
+          Number(currentLetter.letterUserID) >
+          Number(targetLetters[0].letterUserID)
+            ? `${targetLetters[0].letterUserID}_${currentLetter.letterUserID}`
+            : `${currentLetter.letterUserID}_${targetLetters[0].letterUserID}`;
+
+        let match = {
+          user1ID: currentLetter.letterUserID, // 確保存儲 user1ID
+          user2ID: targetLetters[0].letterUserID, // 確保存儲 user2ID
+          user1_ID: currentLetter.letterUser, // 確保存儲 user1_ID
+          user2_ID: targetLetters[0].letterUser, // 確保存儲 user2_ID
+          sendbirdUrl: url,
+          isChecked: false,
+          user1letterContent: currentLetter.letterContent,
+          user2letterContent: targetLetters[0].letterContent,
+          matchUIType: 2,
+        };
+
+        letterMatches.push(match);
+      }
+    }
+
+    await EmotionLetter.deleteMany({
+      letterUserID: { $in: Array.from(consumeUsers) }, // 使用 $in 查詢在 consumeUsers 中的用戶
+    });
+
+    await MatchNewest.insertMany(letterMatches);
+
+    let finishTime = (Date.now() - time) / 1000.0;
+
+    console.log("心情樹洞配對完成", finishTime);
+  } catch (e) {
+    console.log("心情樹洞配對有問題", e);
+  }
+};
+
+runLetterMatch();
