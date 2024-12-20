0d0877e0f41270a2642bae5358c1f6c364b64c4b
Kevin <kevin.lin@leinotek.com>
2024-12-20 17:52:42 +0800

    測試訂閱

--- routes/webhook-route.js
+++ routes/webhook-route.js
@@ -1,28 +1,8 @@
 const router = require("express").Router();
-const { books } = require("googleapis/build/src/apis/books");
 const googleUtil = require("../utils/google-util");
 
-router.post("/google", async (req, res) => {
+router.post("/google-purchase", async (req, res) => {
   try {
-    // Sub/Pub 回傳格式
-    // 參考文獻 ：https://developer.android.com/google/play/billing/rtdn-reference?hl=zh-tw#encoding
-    // message: {
-    //     data: '6YCZ5piv5LiA5YCL5ris6Kmm55qE6KiK5oGv',
-    //     messageId: '13305506657050190',
-    //     message_id: '13305506657050190',
-    //     publishTime: '2024-12-17T08:42:41.576Z',
-    //     publish_time: '2024-12-17T08:42:41.576Z'
-    //     },
-    // subscription: 'projects/onlyfriends-20295/subscriptions/subscription-sub'
-
-    // {"version":"1.0","packageName":"com.anonymous.ipush","eventTimeMillis":"1734430600990","
-    // subscriptionNotification":{"version":"1.0","notificationType":4,
-    // "purchaseToken":"lndfcnmcdjahebbikcnhdokb.AO-J1OwbenosvkJQsvtKKvopSoI6dPP3PezGkK6WskhZFJxZX_WCc9EqHxNcSMKg0EVXAKfKDqElQ0uZeGbjtY6fj8vNgvf3jg",
-    // "subscriptionId":"lovepush_monthly_150"}}
-
-    //退訂的回傳
-    // {"version":"1.0","packageName":"com.anonymous.ipush","eventTimeMillis":"1734606334093","voidedPurchaseNotification":{"purchaseToken":"klklnbpjbipomjneihfpkegc.AO-J1Oy0pl5D_T4_kjt4UFZPOEQyZdVlpnGrEvGhC998-sTvqZ1w04ZqAVRpTFb9Q38hjUt3bjmTa0FRxtuFyMjr3fJqSgUIvw","orderId":"GPA.3346-3431-2321-61213","productType":1,"refundType":1}}
-
     const { message } = req.body;
     let decodeMsg = "";
 
@@ -30,9 +10,9 @@ router.post("/google", async (req, res) => {
       decodeMsg = Buffer.from(message.data, "base64").toString("utf-8");
     }
 
-    //console.log("收到 google 消息", decodeMsg);
-
     let notification = JSON.parse(decodeMsg);
+
+    console.log("receive google purchase MSG : ", decodeMsg);
 
     let subscriptionNotification = notification.subscriptionNotification;
     const voidedPurchaseNotification = notification.voidedPurchaseNotification;
@@ -42,14 +22,61 @@ router.post("/google", async (req, res) => {
       let notificationType = subscriptionNotification.notificationType;
 
       switch (notificationType) {
-        case 4:
-          console.log("訂閱成功！");
-          // 處理訂閱更新或成功的邏輯
+        case "1":
+          console.log(
+            "訂閱項目已從帳戶保留狀態恢復 :SUBSCRIPTION_RECOVERED (1)"
+          );
           break;
-        case 13:
-          console.log("訂閱取消或退款");
-          // 處理訂閱取消或退款的邏輯
+        case "2":
+          console.log("訂閱已續訂:SUBSCRIPTION_RENEWED (2)");
           break;
+        case "3":
+          console.log("自願或非自願取消訂閱: SUBSCRIPTION_CANCELED  (3)");
+          break;
+        case "4":
+          console.log("使用者已購買新的訂閱項目:SUBSCRIPTION_PURCHASED (4)");
+          break;
+        case "5":
+          console.log("訂閱項目已進入帳戶保留狀態: SUBSCRIPTION_ON_HOLD (5)");
+          break;
+        case "6":
+          console.log("訂閱項目已進入寬限期:SUBSCRIPTION_IN_GRACE_PERIOD (6)");
+          break;
+        case "7":
+          console.log(
+            "使用者已從「Play」>「帳戶」>「訂閱」還原訂閱項目。訂閱項目已取消，但在使用者還原時尚未到期: SUBSCRIPTION_RESTARTED (7)"
+          );
+          break;
+        case "8":
+          console.log(
+            "使用者已成功確認訂閱項目價格異動:  SUBSCRIPTION_PRICE_CHANGE_CONFIRMED (8)"
+          );
+          break;
+        case "9":
+          console.log(`訂閱項目的週期時間已延長:SUBSCRIPTION_DEFERRED (9) `);
+          break;
+        case "10":
+          console.log("訂閱項目已暫停: SUBSCRIPTION_PAUSED (10) ");
+          break;
+        case "11":
+          console.log(
+            "訂閱暫停時間表已變更 : SUBSCRIPTION_PAUSE_SCHEDULE_CHANGED (11)"
+          );
+          break;
+        case "12":
+          console.log(
+            " 使用者在訂閱到期前已取消訂閱項目:SUBSCRIPTION_REVOKED (12)"
+          );
+          break;
+        case "13":
+          console.log("訂閱項目已到期:SUBSCRIPTION_EXPIRED (13) ");
+          break;
+        case "20":
+          console.log(
+            "未完成交易被取消 : SUBSCRIPTION_PENDING_PURCHASE_CANCELED (20)"
+          );
+          break;
+
         default:
           console.log(`其他類型的通知: ${notificationType}`);
           break;
@@ -68,9 +95,9 @@ router.post("/google", async (req, res) => {
 
       console.log("驗證結果", result);
     } else if (voidedPurchaseNotification) {
-      // 處理退款或訂單無效的情況
       // 根據 refundType 和 productType 來判斷退款的原因及處理
-      //console.log(`其他類型的通知: ${notificationType}`);
+      //productType : PRODUCT_TYPE_SUBSCRIPTION (1) 訂閱交易已作廢 / PRODUCT_TYPE_ONE_TIME (2) 一次性消費交易已作廢
+      //refundType : REFUND_TYPE_FULL_REFUND (2) 交易已完全作廢 / REFUND_TYPE_QUANTITY_BASED_PARTIAL_REFUND 購買的商品遭到部分商品退款 僅適用於多件購買交易。購買動作可以是 部分作廢項目可能會多次作廢
       console.log("處理退款或訂單無效的情況");
     }
 
@@ -87,4 +114,28 @@ router.post("/google", async (req, res) => {
   }
 });
 
+//iOS
+router.post("/iOS-purchase", async (req, res) => {
+  try {
+    const data = JSON.parse(req.body);
+  } catch (e) {
+    return res.status(500).send({
+      status: false,
+      message: "Server Error!",
+      e,
+    });
+  }
+});
+
 module.exports = router;
+
+//Google Sub/Pub 回傳格式
+//參考文獻 ：https://developer.android.com/google/play/billing/rtdn-reference?hl=zh-tw#encoding
+//message: {
+//     data: '6YCZ5piv5LiA5YCL5ris6Kmm55qE6KiK5oGv',
+//     messageId: '13305506657050190',
+//     message_id: '13305506657050190',
+//     publishTime: '2024-12-17T08:42:41.576Z',
+//     publish_time: '2024-12-17T08:42:41.576Z'
+//     },
+//subscription: 'projects/onlyfriends-20295/subscriptions/subscription-sub'
--- utils/google-util.js
+++ utils/google-util.js
@@ -43,21 +43,74 @@ async function validSubscriptionOrder(
       token: purchaseToken,
     });
 
-    //console.log("訂單驗證成功:", response.data);
-
     return response.data;
   } catch (error) {
-    // console.error(
-    //   "驗證訂單失敗:",
-    //   error.response ? error.response.data : error.message
-    // );
-
     return null;
   }
 }
 
-//validSubscriptionOrder(PACKAGE_NAME, SUBSCRIPTION_ID, PURCHASE_TOKEN);
-
 module.exports = {
   validSubscriptionOrder,
 };
+
+//訂閱回傳結果
+// {"version":"1.0","packageName":"com.anonymous.ipush","eventTimeMillis":"1734430600990","
+// subscriptionNotification":{"version":"1.0","notificationType":4,
+// "purchaseToken":"lndfcnmcdjahebbikcnhdokb.AO-J1OwbenosvkJQsvtKKvopSoI6dPP3PezGkK6WskhZFJxZX_WCc9EqHxNcSMKg0EVXAKfKDqElQ0uZeGbjtY6fj8vNgvf3jg",
+// "subscriptionId":"lovepush_monthly_150"}}
+
+//退訂的回傳
+// {"version":"1.0","packageName":"com.anonymous.ipush","eventTimeMillis":"1734606334093",
+// "voidedPurchaseNotification":{"purchaseToken":"klklnbpjbipomjneihfpkegc.AO-J1Oy0pl5D_T4_kjt4UFZPOEQyZdVlpnGrEvGhC998-sTvqZ1w04ZqAVRpTFb9Q38hjUt3bjmTa0FRxtuFyMjr3fJqSgUIvw",
+// "orderId":"GPA.3346-3431-2321-61213","productType":1,"refundType":1}}
+
+//驗證結果 {
+//    startTimeMillis: '1734608331197',  這是訂閱生效的時間
+//    expiryTimeMillis: '1734608632071', 這是訂閱結束的時間
+//    autoRenewing: false, 訂閱是否會自動續訂
+//    priceCurrencyCode: 'TWD', 訂閱的貨幣代碼
+//    priceAmountMicros: '150000000', 訂閱價格，單位是微元（1 微元 = 1,000,000 分之一的貨幣單位）。
+//    countryCode: 'TW', 購買訂閱的用戶的國家代碼
+//    developerPayload: '',用來存放開發者自定義的資訊
+//    cancelReason: 1, 訂閱取消的原因（如果訂閱被取消）。常見的取消原因有：0: 用戶取消。 1: 付款問題（例如信用卡過期）
+//    orderId: 'GPA.3373-0997-2970-22735', Google Play 訂單的唯一識別碼
+//    purchaseType: 0, 購買的類型 0：表示標準購買
+//    acknowledgementState: 0, 訂閱是否已被確認 0: 訂閱未被確認。 1: 訂閱已被確認
+//   kind: 'androidpublisher#subscriptionPurchase' API 返回的對象類型
+//  }
+
+// 1 (SUBSCRIPTION_RECOVERED):
+// 訂閱從暫停狀態恢復。
+
+// 2 (SUBSCRIPTION_RENEWED):
+// 訂閱自動續訂成功。
+
+// 3 (SUBSCRIPTION_CANCELED):
+// 訂閱被取消，可能是用戶主動取消，也可能是由於支付失敗或其他原因。
+
+// 4 (SUBSCRIPTION_PURCHASED):
+// 用戶進行了新的訂閱購買。
+
+// 5 (SUBSCRIPTION_ON_HOLD):
+// 訂閱因支付問題而被暫停。
+
+// 6 (SUBSCRIPTION_IN_GRACE_PERIOD):
+// 訂閱進入寬限期，通常是因為支付問題，Google Play 會在此期間嘗試收款。
+
+// 7 (SUBSCRIPTION_RESTARTED):
+// 訂閱重新開始，這種情況可能發生在訂閱暫停後被用戶重新激活的情況下。
+
+// 8 (SUBSCRIPTION_PRICE_CHANGE_CONFIRMED):
+// 用戶確認了訂閱價格的變更。
+
+// 9 (SUBSCRIPTION_DEFERRED):
+// 訂閱延期，可能是由於某些原因推遲了下一次續訂的日期。
+
+// 10 (SUBSCRIPTION_PAUSED):
+// 訂閱被暫停，可能是用戶主動暫停了訂閱。
+
+// 11 (SUBSCRIPTION_REVOKED):
+// 訂閱被撤銷，可能是開發者或 Google Play 撤銷了訂閱。
+
+// 12 (SUBSCRIPTION_EXPIRED):
+// 訂閱過期，未成功續訂。
