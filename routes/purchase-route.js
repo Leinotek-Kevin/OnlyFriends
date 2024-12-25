7c072b374f35990f6dd4eed938326ddbf5a2e881
Kevin <kevin.lin@leinotek.com>
2024-12-25 17:11:13 +0800

    新增C-3 查看用戶購買紀錄

--- routes/purchase-route.js
+++ routes/purchase-route.js
@@ -176,6 +176,39 @@ router.post("/iOS-verify", async (req, res) => {
   }
 });
 
+//C-3 查詢用戶購買紀錄
+router.post("/query-history", async (req, res) => {
+  try {
+    let { userID } = req.user;
+    let { productType } = req.body;
+
+    let data = [];
+
+    if (productType) {
+      data = await Purchase.find({
+        userID,
+        productType: productType == "0" ? "subscription" : "in-app",
+      }).sort({ createDate: -1 });
+    } else {
+      data = await Purchase.find({
+        userID,
+      }).sort({ createDate: -1 });
+    }
+
+    return res.status(200).send({
+      status: true,
+      message: "獲取用戶購買紀錄",
+      data,
+    });
+  } catch (e) {
+    return res.status(500).send({
+      status: false,
+      message: "Serror Error!",
+      e,
+    });
+  }
+});
+
 //取得驗證購買紀錄
 router.post("/verify-receipt", async (req, res) => {
   try {
