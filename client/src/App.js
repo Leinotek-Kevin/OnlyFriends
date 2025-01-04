8e3841984d08cbd7be27269f62e255fa06f794a6
Kevin <kevin.lin@leinotek.com>
2025-01-04 23:09:07 +0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeComponent from "./components/home-component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          {/* outlet 展示首次進來的第一個 index 畫面是 HomeComponent */}
+          <Route index element={<HomeComponent />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
