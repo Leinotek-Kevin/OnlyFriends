003e99bcd7d1209c42b0576c9f709fd9120afc09
Kevin <kevin.lin@leinotek.com>
2025-02-26 16:23:45 +0800

    HK4G4

--- service/alert-match-service.js
+++ service/alert-match-service.js
@@ -1,12 +1,19 @@
 const sbUtil = require("../utils/sendbird-util");
+const cloudStore = require("../utils/cloudStore-util");
 
 //提醒午夜新配對要開始了 23:30
 const runAlertMatch = async () => {
   try {
-    const result = await sbUtil.sendMsgOpenChannel(
-      "午夜新配對快要到嚕！",
-      "matchAlert"
-    );
+    // const result = await sbUtil.sendMsgOpenChannel(
+    //   "午夜新配對快要到嚕！",
+    //   "matchAlert"
+    // );
+    const result = await cloudStore.addMessage({
+      customType: "matchAlert",
+      msg: "午夜新配對快要到嚕！",
+      link: "",
+      image: "",
+    });
     console.log("午夜新配對快要到嚕！", result ? "發送完畢" : "發送有問題");
   } catch (e) {
     console.log("午夜提醒訊息發送失敗", e);
