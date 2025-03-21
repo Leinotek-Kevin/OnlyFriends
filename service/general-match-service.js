4c29bcaad9dcc528b856aa68233ddb1e500fe618
Kevin <kevin.lin@leinotek.com>
2025-03-22 00:17:58 +0800

    修正

--- service/general-match-service.js
+++ service/general-match-service.js
@@ -15,7 +15,7 @@ const cloudStorage = require("../utils/cloudStorage-util");
 const generalMatch = async () => {
   const now = new Date();
 
-  if (now.getHours() == 16 && now.getMinutes() == 28) {
+  if (now.getHours() == 0 && now.getMinutes() == 20) {
     const RE_MATCH_DELAY = 48 * 60 * 60 * 1000;
     const time48HoursAgo = Date.now() - RE_MATCH_DELAY; // 計算48小時前的時間點
     const lastNightTimeStamp = dateUtil.getYesterdayNight();
