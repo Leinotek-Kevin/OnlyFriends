463d9d2689cfd21dc8bb57faa95cfa8c69e0d6f4
Kevin <kevin.lin@leinotek.com>
2025-03-28 17:43:47 +0800

    新增照片審核機制

--- routes/other-route.js
+++ routes/other-route.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 const User = require("../models").user;
 const Config = require("../models").config;
 const generalUtil = require("../utils/general-util");
+const visionUtil = require("../utils/google-vision-util");
 
+//查詢指定用戶
 router.post("/query-user", async (req, res) => {
   try {
     let { userID } = req.body;
@@ -93,7 +95,66 @@ router.post("/curd-version", async (req, res) => {
       });
     }
   } catch (e) {
-    console.log(e);
+    return res.status(500).send({
+      status: false,
+      message: "Server Error!",
+      e,
+    });
+  }
+});
+
+//檢查註冊照片是否符合規範
+router.post("/check-photo", async (req, res) => {
+  try {
+    const { photo } = req.body;
+
+    if (!generalUtil.isNotNUllEmpty(photo)) {
+      return res.status(200).send({
+        status: true,
+        message: "請提供要檢查的圖片網址",
+        data: null,
+      });
+    }
+
+    //先檢查是否合規
+    const imageResult = await visionUtil.checkImageForIllegal(photo);
+
+    if (!imageResult.isSafe) {
+      //違規照片
+      return res.status(200).send({
+        status: true,
+        message: "您的照片已違反 Only Friends 使用者規範！",
+        data: {
+          checkCode: 101,
+          checkMessage: imageResult.reason,
+        },
+      });
+    }
+
+    //在檢查是否是個人
+    const faceResult = await visionUtil.checkImageForHumanFace(photo);
+
+    if (!faceResult.isSafe) {
+      //不是一個人臉
+      return res.status(200).send({
+        status: true,
+        message: "您上傳的照片不符合真實人臉特徵",
+        data: {
+          checkCode: 102,
+          checkMessage: faceResult.reason,
+        },
+      });
+    }
+
+    return res.status(200).send({
+      status: true,
+      message: "您的照片已通過審核！",
+      data: {
+        checkCode: 100,
+        checkMessage: "This photo has been approved!",
+      },
+    });
+  } catch (e) {
     return res.status(500).send({
       status: false,
       message: "Server Error!",
---
+++ utils/google-vision-util.js
@@ -0,0 +1,88 @@
+const fs = require("fs");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 8080;
+const vision = require("@google-cloud/vision");
+
+const isLocal = port == 8080;
+let keyFilePath;
+
+if (isLocal) {
+  keyFilePath = path.join(__dirname, "../onlyfriends-developer.json"); // 本地開發使用本地憑證文件
+} else {
+  const serviceAccount = JSON.parse(process.env.GOOGLE_DEVELOPER_ACCOUNT);
+  keyFilePath = path.join(__dirname, "temp-google-service-account.json");
+
+  // 寫入臨時檔案
+  fs.writeFileSync(keyFilePath, JSON.stringify(serviceAccount));
+}
+
+// 初始化 Google Cloud Vision 客戶端
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: keyFilePath,
+});
+
+//檢查圖片中的不當內容
+async function checkImageForIllegal(imageUrl) {
+  try {
+    // 發送塗片 URL 給 Vision API 進行審查
+    const [result] = await client.safeSearchDetection(imageUrl);
+    const detections = result.safeSearchAnnotation;
+
+    // 檢查是否包含成人內容或暴力或醫療
+    if (detections.adult === "LIKELY" || detections.adult === "VERY_LIKELY") {
+      return { isSafe: false, reason: "Adult content detected" };
+    }
+    if (
+      detections.violence === "LIKELY" ||
+      detections.violence === "VERY_LIKELY"
+    ) {
+      return { isSafe: false, reason: "Violent content detected" };
+    }
+
+    if (
+      detections.medical === "LIKELY" ||
+      detections.medical === "VERY_LIKELY"
+    ) {
+      return { isSafe: false, reason: "Medical content detected" };
+    }
+
+    // 如果沒有不當內容，返回安全標示
+    return { isSafe: true };
+  } catch (error) {
+    return { isSafe: false, reason: "Error during content check" };
+  }
+}
+
+async function checkImageForHumanFace(imageUrl) {
+  let result = { isFace: false, reason: "No face detected" };
+
+  try {
+    // 發送圖片 URL 給 Vision API 進行審查
+    const [faceResult] = await client.faceDetection(imageUrl);
+
+    // 取得回應中的人臉標註
+    const faces = faceResult.faceAnnotations;
+
+    if (faces.length > 0) {
+      faces.forEach((face, index) => {
+        if (face.detectionConfidence >= 0.7) {
+          result.isFace = true;
+          result.reason = "This face is Human ";
+          result.detectionConfidence = face.detectionConfidence;
+        }
+      });
+    }
+
+    return result;
+  } catch (error) {
+    result.reason = "Error during face detection";
+    return result;
+  }
+}
+
+module.exports = {
+  checkImageForIllegal,
+  checkImageForHumanFace,
+};
