a2eccbaf55fbfbbb8bb9863392b51bda02c4d15a
Kevin <kevin.lin@leinotek.com>
2025-04-22 17:14:28 +0800

    檢查用戶大頭貼是否是人

--- routes/other-route.js
+++ routes/other-route.js
@@ -156,4 +156,64 @@ router.post("/check-photo", async (req, res) => {
     });
   }
 });
+
+//檢查目前用戶大頭貼不是人, 並置換成 警示圖片
+router.post("/check-user-real", async (req, res) => {
+  try {
+    const pLimit = (await import("p-limit")).default;
+    const limit = pLimit(5); // 限制同時處理 5 個
+
+    const aliveUser = await User.find({ identity: 2, userValidCode: "1" });
+
+    const checkPromises = aliveUser.map((user) =>
+      limit(async () => {
+        const userPhoto = user.userPhotos;
+        if (userPhoto && userPhoto.length > 0) {
+          try {
+            const result = await visionUtil.checkImageForHumanFace(
+              userPhoto[0]
+            );
+
+            return {
+              userId: user.userID,
+              hasFace: result.isFace,
+            };
+          } catch (error) {
+            return {
+              userId: user.userID,
+              hasFace: false,
+            };
+          }
+        }
+      })
+    );
+
+    const results = await Promise.all(checkPromises);
+
+    const notRealUserIds = results
+      .filter((result) => result?.hasFace === false)
+      .map((result) => result.userId);
+
+    const warningImageUrl =
+      "https://firebasestorage.googleapis.com/v0/b/onlyfriends-20295.appspot.com/o/system%2Frobot-photo%2Fother%2Fuser-photo-warning.png?alt=media&token=adccf915-3f13-410d-ba7e-27760240a5b7";
+
+    //user-photo-warning 警告要換大頭貼
+    await User.updateMany(
+      { userID: { $in: notRealUserIds } },
+      { $set: { "userPhotos.0": warningImageUrl } }
+    );
+
+    return res.status(200).send({
+      status: true,
+      message: "辨識已完成,並完成置換警示大頭貼",
+      notRealUserIds,
+    });
+  } catch (e) {
+    return res.status(500).send({
+      status: false,
+      message: "Server Error!",
+    });
+  }
+});
+
 module.exports = router;
