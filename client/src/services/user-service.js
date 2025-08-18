9c0b0b361f663381cb36ac8d15d4dbe364ccfd1a
Kevin <kevin.lin@leinotek.com>
2025-08-18 22:37:03 +0800

    前端改為正式環境

--- client/src/services/user-service.js
+++ client/src/services/user-service.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import dayjs from "dayjs";
 
 //Web 本地
-const BASE_URL = "http://localhost:8080/api/user";
+//const BASE_URL = "http://localhost:8080/api/user";
 //開發環境
 //const BASE_URL = "https://dev.ofs.leinotek.com/api/user";
 //正式環境
-//const BASE_URL = "https://ofs.leinotek.com/api/user";
+const BASE_URL = "https://ofs.leinotek.com/api/user";
 
 class UserService {
   //加入聯盟行銷夥伴
